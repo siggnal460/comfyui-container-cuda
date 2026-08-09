31df05a3aa5e0fb864001fe39c82373a1929edd1
siggnal460 <siggnal@proton.me>
2026-08-09 15:03:47 -0600

    Added --break-system-packages to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -43,7 +43,7 @@ for custom_node_directory in /app/custom_nodes/*; do
     custom_node_name=${custom_node_directory##*/}
     custom_node_name=${custom_node_name//[-_]/ }
     emphatic_echo "Installing requirements for $custom_node_name..."
-    python -m pip install --root-user-action=ignore --requirement "$custom_node_directory/requirements.txt" 2> >(while read line; do echo -e "\e[31m$line\e[0m"; done)
+    python -m pip install --root-user-action=ignore --break-system-packages --requirement "$custom_node_directory/requirements.txt" 2> >(while read line; do echo -e "\e[31m$line\e[0m"; done)
   fi
 done
 
